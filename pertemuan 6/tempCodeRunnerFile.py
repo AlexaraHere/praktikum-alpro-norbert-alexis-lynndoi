2e074d9468574069d9ddeab0cda6b1da1a866c6c
alexara <bebe.norbert1@gmail.com>
2025-03-12 13:10:30 +0700

    hoaaaaaaaaaaaaaaa

--- pertemuan 6/tempCodeRunnerFile.py
+++ pertemuan 6/tempCodeRunnerFile.py
@@ -1,13 +1,2 @@
-
-# def factorial(n):
-#     a = 1
-#     print(f"{n}! =", end=" ")  # Menampilkan notasi faktorial
-#     for i in range(1, n+1):  # Mulai dari 1 sampai n
-#         a *= i  # Menghitung faktorial
-#         print(i, end=" ")  # Menampilkan angka
-#         if i < n:
-#             print("*", end=" ")  # Menampilkan tanda kali
-#     print(f"= {a}")  # Menampilkan hasil akhir
-
-# # Contoh penggunaan:
-# factorial(5)
+print("Papan Catur:")
+papan_catur(n)
--- pertemuan 6/testing.py
+++ pertemuan 6/testing.py
@@ -83,14 +83,14 @@
 #             print("*", end="")
 #         print()
 
-# def papan_catur(n):
-#     for i in range(n):
-#         for j in range(n):
-#             if (i + j) % 2 == 0:
-#                 print("#", end=" ")
-#             else:
-#                 print(" ", end=" ")
-#         print()
+def papan_catur(n):
+    for i in range(n):
+        for j in range(n):
+            if (i + j) % 2 == 0:
+                print("#", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
 
 # n = int(input("ya "))
 # print("Piramida Angka:")
@@ -102,8 +102,8 @@
 # print("Segitiga Terbalik:")
 # segitiga_terbalik(n)
 
-# print("Papan Catur:")
-# papan_catur(n)
+print("Papan Catur:")
+papan_catur(5)
 
 # aaaaa = int(input("masukin angka perkalian 1 "))
 # bbbbb = int(input("masukin angka perkalian 2 "))   
@@ -155,3 +155,54 @@
 # for i in range(int(n)):
 #     tup = (tup,)
 #     print(tup)
+
+
+
+# def hitung_total_harga(jumlah_barang):
+#     total_harga = 0
+#     for i in range(1, jumlah_barang + 1):
+#         harga = int(input(f"Masukkan harga barang ke-{i}: "))
+#         total_harga += harga
+#     return total_harga
+
+# # Input jumlah barang
+# jumlah_barang = int(input("Masukkan jumlah barang: "))
+
+# # Hitung total harga menggunakan fungsi
+# total_harga = hitung_total_harga(jumlah_barang)
+
+# # Tampilkan hasil
+# print(f"Total harga belanjaan: {total_harga}")
+
+
+
+
+
+# def fibonacci(n):
+#     a, b = 0, 1
+#     for bebe in range(n - 1):
+#         a, b = b, a + b
+#     return b
+
+# def digit_pertama(x):
+#     while x >= 10:
+#         x //= 10
+#     return x
+
+# # Input dari pengguna
+# n = int(input("Masukkan nilai n: "))
+
+# # Hitung Fibonacci ke-n
+# fib_n = fibonacci(n)
+
+# # Cetak hasil
+# print(f"Bilangan Fibonacci ke-{n} adalah {fib_n}")
+# print(f"Digit pertama dari {fib_n} adalah {digit_pertama(fib_n)}")
+
+
+
+
+
+
+
+
